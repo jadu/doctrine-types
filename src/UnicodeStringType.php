bb270871ec92dfbd34df8183dda530c503360f05
jamesm <james.manley@jadu.netk>
2016-10-05 12:14:06 +0100

    Inital commit

---
+++ src/UnicodeStringType.php
@@ -0,0 +1,36 @@
+<?php
+namespace Jadu\DoctrineTypes;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+/**
+ * My custom datatype.
+ */
+class UnicodeStringType extends Type
+{
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        if ($platform->getName() == 'mysql') {
+            return 'VARCHAR(' . (!is_null($fieldDeclaration['length']) ? intval($fieldDeclaration['length']) : '255') . ') COMMENT \'(DC2Type:unicodestring)\'';
+        }
+        else {
+            return 'NVARCHAR(' . (!is_null($fieldDeclaration['length']) ? intval($fieldDeclaration['length']) : '255') . ') COMMENT \'(DC2Type:unicodestring)\'';
+        }
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    public function getName()
+    {
+        return 'unicodestring';
+    }
+}
---
+++ src/UnicodeTextType.php
@@ -0,0 +1,36 @@
+<?php
+namespace Jadu\DoctrineTypes;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+/**
+ * My custom datatype.
+ */
+class UnicodeTextType extends Type
+{
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        if ($platform->getName() == 'mysql') {
+            return 'TEXT COMMENT \'(DC2Type:unicodetext)\'';
+        }
+        else {
+            return 'NVARCHAR(MAX) COMMENT \'(DC2Type:unicodetext)\'';
+        }
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    public function getName()
+    {
+        return 'unicodetext';
+    }
+}
